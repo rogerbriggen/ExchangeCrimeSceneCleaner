5b9936405bb30cd5fa7fe6c23747b121e579e994
Roger Briggen <roger.briggen@bluewin.ch>
2021-12-10 11:47:32 +0100

    empty project

---
+++ src/ExchangeCrimeSceneCleanerLib/QueryContacts.cs
@@ -0,0 +1,9 @@
+﻿// Roger Briggen license this file to you under the MIT license.
+//
+
+namespace ExchangeCrimeSceneCleanerLib;
+
+public class QueryContacts
+{
+    public const string ContactURL = "contacts?";
+}
